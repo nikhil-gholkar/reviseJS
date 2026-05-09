42b249d34c6f541ae285d67d40263a68e64fbfa8
Nikhil Gholakar <nikhil.gholkar76@gmail.com>
2026-05-09 14:28:10 +0530

    inbuilt classes

---
+++ map.js
@@ -0,0 +1,18 @@
+
+const map= new Map()
+
+map.set("name","nikhil")
+map.set("age",25)
+console.log(map.get("name"))
+
+const user={
+
+}
+
+user.name="nikhil"
+user.age=25
+
+console.log(`name from object ${user.name}`)
+
+const now = new Date()
+console.log(now.toTimeString())
